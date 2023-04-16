8580787741a58f0c324523a7fd610b4d91867c10
SimDen7 <124933402+SimDen7@users.noreply.github.com>
2023-04-16 16:50:20 -0400

    Add files via upload

---
+++ app/src/main/java/com/example/sudoku/view/LeaderboardActivity.kt
@@ -0,0 +1,44 @@
+package com.example.sudoku.view
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sudoku.R
+import com.example.sudoku.databinding.ActivityLeaderboardBinding
+
+class LeaderboardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLeaderboardBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLeaderboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val getIntent = intent
+        val listLeaderboard = findViewById<ListView>(R.id.ListViewLeaderboard)
+
+        val getName = intent.getStringArrayListExtra("dataName")
+        val getTime = intent.getStringArrayListExtra("dataTime")
+        val getErrors = intent.getStringArrayListExtra("dataErrors")
+        val getAllData = ArrayList<String>()
+        var index = 0
+
+        while ((index < getName!!.size) and (index < getTime!!.size) and (index < getErrors!!.size)) {
+            getAllData.add("${index+1} " + getName[index] + " " + getTime[index] + " " + getErrors[index])
+            index++
+        }
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, getAllData)
+        listLeaderboard.adapter = arrayAdapter
+        binding.BackButton.setOnClickListener {
+            super.finish()
+        }
+
+        binding.OnlineButton.setOnClickListener{
+            //online leaderboards
+        }
+
+    }
+
+}
