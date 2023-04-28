ba68bd25d25f18100e2c98b57e847bcb8b2341eb
SimDen7 <124933402+SimDen7@users.noreply.github.com>
2023-04-28 03:58:55 -0400

    Update User.kt

--- app/src/main/java/com/example/sudoku/viewModel/User.kt
+++ app/src/main/java/com/example/sudoku/viewModel/User.kt
@@ -4,12 +4,12 @@ import android.widget.EditText
 data class User(
     val nom: String,
     val prenom: String,
+    val temps: String? = null,
+    val erreurs: Int? = null,
     val score: Int?= null,
     val nomdutilisateur: String = "",
     val uid: String = "",
-    var imageUrl:String= " "
+    var imageUrl:String=""
 ) {
-
-
     constructor() : this("", "", null, "", "")
 }
