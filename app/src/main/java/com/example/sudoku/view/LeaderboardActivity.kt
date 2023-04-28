706cade131ad60702536a72e2fdd7d4a6b63c5ee
SimDen7 <124933402+SimDen7@users.noreply.github.com>
2023-04-27 21:13:43 -0400

    Update LeaderboardActivity.kt

--- app/src/main/java/com/example/sudoku/view/LeaderboardActivity.kt
+++ app/src/main/java/com/example/sudoku/view/LeaderboardActivity.kt
@@ -31,12 +31,53 @@ class LeaderboardActivity : AppCompatActivity() {
 
         val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, getAllData)
         listLeaderboard.adapter = arrayAdapter
+        
+        val getOnlineData = ArrayList<String>()
+        /*
+        val userRef = FirebaseDatabase.getInstance().getReference("Users").orderByKey()
+        userRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                for (postSnapshot in dataSnapshot.children) {
+
+                    var x = 1
+
+                    val Username = dataSnapshot.child("nomdutilisateur").getValue(String::class.java)
+                    val Nom = dataSnapshot.child("nom").getValue(String::class.java)
+                    val Prenom = dataSnapshot.child("prenom").getValue(String::class.java)
+                    val Score = dataSnapshot.child("score").getValue(Int::class.java)
+                    getOnlineData.add("$x " + "$Username " + "$Score")
+                    x++
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+        val arrayAdapterOnline = ArrayAdapter(this, android.R.layout.simple_list_item_1, getOnlineData)
+
+         */
+        
         binding.BackButton.setOnClickListener {
             super.finish()
         }
 
         binding.OnlineButton.setOnClickListener{
-            //online leaderboards
+            
+            if(binding.OnlineButton.text == "Online Leaderboard") {
+
+
+
+                listLeaderboard.adapter = arrayAdapterOnline
+                binding.OnlineButton.text = "Personnal Leaderboard"
+                binding.TextViewLeaderboard.text = "Online Leaderboard"
+            }
+            else {
+                listLeaderboard.adapter = arrayAdapterLocal
+                binding.OnlineButton.text = "Online Leaderboard"
+                binding.TextViewLeaderboard.text = "Personnal Leaderboard"
+            }
         }
 
     }
